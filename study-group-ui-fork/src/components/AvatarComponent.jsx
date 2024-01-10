9c478ea105dca142da51df0aed79e19a94c909d1
builinht <builinhaqua@gmail.com>
2024-01-10 17:28:49 +0700

    dele

--- study-group-ui-fork/src/components/AvatarComponent.jsx
+++
@@ -1,9 +0,0 @@
-import { Avatar, styled } from '@mui/material';
-
-export default styled(Avatar)(({ theme }) => ({
-  width: '100px',
-  height: '100px',
-  cursor: 'pointer',
-  textAlign: 'center',
-  backgroundColor: theme.palette.darkGreen.main
-}));
--- study-group-ui-fork/src/services/api/parents/StatApi.js
+++
@@ -1,9 +0,0 @@
-import client from "src/common/client";
-import { API_PARENTS_STATS, API_PAST_MEETINGS } from "src/common/constants";
-
-const PARENT_STAT_API = {
-  GET_STUDENT_BY_PARENTS: () => client.get(API_PARENTS_STATS),
-  GET_PAST_MEETINGS: (dayTime) => client.get(`${API_PAST_MEETINGS}/${dayTime}`),
-};
-
-export default PARENT_STAT_API;
