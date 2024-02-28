3fc762d825877a0704b102e35e15e872ee67dc69
builinht <90827474+builinht@users.noreply.github.com>
2024-02-28 21:17:08 +0700

    create discussion

---
+++ fe-wilearn/src/components/AddDiscussion.jsx
@@ -0,0 +1,141 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+export default function AddDiscussion() {
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState('');
+  const [newTopic, setNewTopic] = useState('');
+  const [showNewPost, setShowNewPost] = useState(false);
+  const [formattedDate, setFormattedDate] = useState('');
+  const [formattedTime, setFormattedTime] = useState('');
+  const currentUser = 'Anonymous';
+
+  const handlePostChange = (value) => {
+    setNewPost(value);
+  };
+
+  const handleClose = () => {
+    setShowNewPost(false);
+  };
+  
+  const handleTopicChange = (event) => {
+    setNewTopic(event.target.value);
+  };
+
+  const handlePostSubmit = () => {
+    if (newPost.trim() !== '' && newTopic.trim() !== '') {
+      const currentDate = new Date();
+      const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+      const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+      setFormattedDate(currentDate.toLocaleDateString(undefined, dateOptions));
+      setFormattedTime(currentDate.toLocaleTimeString(undefined, timeOptions));
+
+      const newPostWithTime = {
+        content: newPost,
+        date: formattedDate,
+        time: formattedTime,
+        topic: newTopic,
+        creator: currentUser,
+      };
+
+      setPosts([...posts, newPostWithTime]);
+      setNewPost('');
+      setNewTopic('');
+
+      setShowNewPost(true);
+    }
+  };
+
+  return (
+    <div style={{maxWidth:'900px', margin:'0 auto', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+      <div>
+        <div style={{ textAlign: 'center' }}>
+          <h2>Add Discussion</h2>
+        </div>
+        <input
+          type="text"
+          value={newTopic}
+          onChange={handleTopicChange}
+          placeholder="Enter topic..."
+          style={{ marginBottom: '5px', padding: '5px', marginLeft:'0px' }}
+        />
+        <ReactQuill
+          value={newPost}
+          onChange={handlePostChange}
+          placeholder="Write your post here..."
+          modules={{
+            toolbar: [
+              [{ header: [1, 2, false] }],
+              ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+              [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+              ['link', 'image'],
+              ['clean'],
+            ],
+          }}
+          formats={[
+            'header',
+            'bold', 'italic', 'underline', 'strike', 'blockquote',
+            'list', 'bullet', 'indent',
+            'link', 'image',
+          ]}
+          style={{height:'200px'}}
+        />
+        <br />
+        <div style={{ marginTop: '50px' }}>
+        <button
+          onClick={handlePostSubmit}
+          style={{
+            backgroundColor: '#4CAF50',
+            color: 'white',
+            padding: '10px 20px',
+            cursor: 'pointer',
+            border: 'none',
+            borderRadius: '5px',
+            fontSize: '16px',
+            marginLeft: '50px',
+            transition: 'background-color 0.3s', 
+          }}
+          onMouseOver={(e) => (e.target.style.backgroundColor = '#45a049')}
+          onMouseOut={(e) => (e.target.style.backgroundColor = '#4CAF50')}
+        >
+          Submit
+        </button>
+        <button
+            onClick={handleClose}
+            style={{
+            backgroundColor: '#999999',
+            color: 'white',
+            padding: '10px 20px',
+            cursor: 'pointer',
+            border: 'none',
+            borderRadius: '5px',
+            fontSize: '16px',
+            transition: 'background-color 0.3s',
+            marginLeft: '600px'
+            }}
+            onMouseOver={(e) => (e.target.style.backgroundColor = '#666666')}
+            onMouseOut={(e) => (e.target.style.backgroundColor = '#999999')}
+        >
+            Close
+        </button>
+        </div>
+        </div>
+            <div>
+                {showNewPost && (
+                  <div style={{ border: '1px solid #ccc', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                    <div style={{ background: '#f8f8f8', padding: '20px', borderBottom: '1px solid #ccc' }}>
+                      <h2 style={{ fontSize: '20px', margin: '0', color: '#333' }}>Topic: {posts[posts.length - 1].topic}</h2>
+                      <p style={{ fontSize: '14px', color: '#888', margin: '5px 0' }}>Date: {formattedDate}, Time: {formattedTime}</p>
+                    </div>
+                    <div style={{ padding: '20px' }}>
+                      <div dangerouslySetInnerHTML={{ __html: posts[posts.length - 1].content }} />
+                      <p style={{ fontSize: '14px', color: '#888', marginTop: '10px' }}>Created by: {currentUser}</p>
+                    </div>
+                  </div>
+                )}
+            </div>
+    </div>
+  );
+}
--- fe-wilearn/src/components/Discussion.jsx
+++ fe-wilearn/src/components/Discussion.jsx
@@ -1,3 +1,86 @@
-export default function Discussioon(){
-    return <h1>Discussion component</h1>
+import { useState } from 'react';
+import AddDiscussion from './AddDiscussion';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+export default function Discussion() {
+  const [showDiscussion, setAddDiscussion] = useState(false);
+
+  const handleClick = () => {
+    setAddDiscussion(true);
+  };
+
+  const closeModal = () => {
+    setAddDiscussion(false);
+  };
+
+  return (
+    <div>
+      <h1>Discussion</h1>
+      <div>
+        <Popup
+          modal
+          open={showDiscussion}
+          onClose={closeModal}
+          contentStyle={{
+            width: '60%',
+            height: '60%',
+            borderRadius: '10px',
+          }}
+        >
+          <AddDiscussion discussionId={1} onClose={closeModal} />
+        </Popup>
+        <button
+          onClick={handleClick}
+          style={{
+            textAlign: 'center',
+            cursor: 'pointer',
+            padding: '10px 20px',
+            fontSize: '16px',
+            backgroundColor: '#4CAF50',
+            color: 'white',
+            border: 'none',
+            borderRadius: '5px',
+            marginLeft: '900px',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          + Add Discussion
+        </button>
+      </div>
+      <div style={{ border: '1px solid #ccc', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', maxWidth:'900px',margin: '20px auto 0' }}>
+        <div style={{ background: '#f8f8f8', padding: '20px', borderBottom: '1px solid #ccc' }}>
+          <h2 style={{ fontSize: '20px', margin: '0', color: '#333' }}>Topic: How to add posts using React?</h2>
+          <p style={{ fontSize: '14px', color: '#888', margin: '5px 0' }}>Date: 28/02/2023 Time: 12:03:15</p>
+        </div>
+        <div style={{ padding: '20px' }}>
+            <p style={{ fontSize: '14px', color: '#888', textAlign: 'justify' }}>
+            We use HTTP requests, such as POST, to “talk to” APIs over the web. With HTTP requests, we can access resources outside of our own domain (where our web app is stored). These resources can include files, databases, compute functions, and more. Besides added capability, using outside resources allows us to make data accessible to all instances of our web app. For example, if we want to keep track of user data for logins, by putting this data in a database in the cloud, all instances of our web app can access it from the client side.
+            </p>
+            <p style={{ fontSize: '14px', color: '#555', marginTop: '10px', textAlign: 'left', fontWeight: 'bold', fontStyle: 'italic', background: '#eee', padding: '5px', borderRadius: '5px' }}>Created by: Phuong Nam</p>        </div>
+      </div>
+      <div style={{ border: '1px solid #ccc', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', maxWidth:'900px',margin: '20px auto 0' }}>
+        <div style={{ background: '#f8f8f8', padding: '20px', borderBottom: '1px solid #ccc' }}>
+          <h2 style={{ fontSize: '20px', margin: '0', color: '#333' }}>Topic: How to add posts using React?</h2>
+          <p style={{ fontSize: '14px', color: '#888', margin: '5px 0' }}>Date: 28/02/2023 Time: 12:03:15</p>
+        </div>
+        <div style={{ padding: '20px' }}>
+            <p style={{ fontSize: '14px', color: '#888', textAlign: 'justify' }}>
+            We use HTTP requests, such as POST, to “talk to” APIs over the web. With HTTP requests, we can access resources outside of our own domain (where our web app is stored). These resources can include files, databases, compute functions, and more. Besides added capability, using outside resources allows us to make data accessible to all instances of our web app. For example, if we want to keep track of user data for logins, by putting this data in a database in the cloud, all instances of our web app can access it from the client side.
+            </p>
+            <p style={{ fontSize: '14px', color: '#555', marginTop: '10px', textAlign: 'left', fontWeight: 'bold', fontStyle: 'italic', background: '#eee', padding: '5px', borderRadius: '5px' }}>Created by: Phuong Nam</p>        </div>
+      </div>
+      <div style={{ border: '1px solid #ccc', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', maxWidth:'900px',margin: '20px auto 0' }}>
+        <div style={{ background: '#f8f8f8', padding: '20px', borderBottom: '1px solid #ccc' }}>
+          <h2 style={{ fontSize: '20px', margin: '0', color: '#333' }}>Topic: How to add posts using React?</h2>
+          <p style={{ fontSize: '14px', color: '#888', margin: '5px 0' }}>Date: 28/02/2023 Time: 12:03:15</p>
+        </div>
+        <div style={{ padding: '20px' }}>
+            <p style={{ fontSize: '14px', color: '#888', textAlign: 'justify' }}>
+            We use HTTP requests, such as POST, to “talk to” APIs over the web. With HTTP requests, we can access resources outside of our own domain (where our web app is stored). These resources can include files, databases, compute functions, and more. Besides added capability, using outside resources allows us to make data accessible to all instances of our web app. For example, if we want to keep track of user data for logins, by putting this data in a database in the cloud, all instances of our web app can access it from the client side.
+            </p>
+            <p style={{ fontSize: '14px', color: '#555', marginTop: '10px', textAlign: 'left', fontWeight: 'bold', fontStyle: 'italic', background: '#eee', padding: '5px', borderRadius: '5px' }}>Created by: Phuong Nam</p>        </div>
+      </div>
+    </div>
+  );
 }
