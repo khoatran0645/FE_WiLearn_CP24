e31cb33f2bd4a9fd754474c5f4e7e46caf013a0f
builinht <90827474+builinht@users.noreply.github.com>
2024-02-29 02:05:09 +0700

    add schedule

--- fe-wilearn/src/components/Schedule.jsx
+++ fe-wilearn/src/components/Schedule.jsx
@@ -1,3 +1,38 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 export default function Schedule(){
-    return <h1>Schedule component</h1>
+    const localizer = momentLocalizer(moment);
+
+  const events = [
+    {
+      title: 'Math Class',
+      start: new Date(2024, 2, 1, 10, 0),
+      end: new Date(2024, 2, 1, 12, 0),
+    },
+    {
+      title: 'English Class',
+      start: new Date(2024, 2, 2, 14, 0),
+      end: new Date(2024, 2, 2, 16, 0),
+    },
+    {
+      title: 'Science Class',
+      start: new Date(2024, 2, 3, 13, 0),
+      end: new Date(2024, 2, 3, 15, 0),
+    },
+
+  ];
+
+  return (
+    <div style={{maxWidth:'1200px', marginLeft:'20px'}}>
+      <h1>My Schedule</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
 }
