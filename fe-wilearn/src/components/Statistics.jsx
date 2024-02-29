84d8f13fb344b4729b07dd8cbc01b88be2fa6c63
builinht <90827474+builinht@users.noreply.github.com>
2024-02-29 11:20:50 +0700

    fix Statistics

--- fe-wilearn/src/components/Statistics.jsx
+++ fe-wilearn/src/components/Statistics.jsx
@@ -1,56 +1,12 @@
-import { Bar } from 'react-chartjs-2';
-import 'chart.js/auto';
+import ChartMeeting from "./ChartMeeting";
+import StatisticItem from "./StatisticItem";
 
-const Statistics = () => {
-  const data = [
-    { month: 'Jan', count: 1 },
-    { month: 'Feb', count: 0 },
-    { month: 'Mar', count: 2 },
-    { month: 'Apr', count: 5 },
-    { month: 'May', count: 3 },
-    { month: 'Jun', count: 1 },
-    { month: 'Jul', count: 0 },
-    { month: 'Aug', count: 4 },
-    { month: 'Sep', count: 2 },
 
-  ];
-
-  const chartData = {
-    labels: data.map(row => row.month),
-    datasets: [
-      {
-        label: 'Hours',
-        data: data.map(row => row.count),
-        backgroundColor: '#ECAB53',
-        borderColor: '#ECAB53',
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const chartOptions = {
-    scales: {
-      x: {
-        type: 'category',
-        labels: data.map(row => row.month),
-      },
-      y: {
-        beginAtZero: true,
-        title: {
-          display: true,
-          text: 'Hours',
-        },
-      },
-    },
-  };
-
+export default function Statistics() {
   return (
-    <div style={{maxWidth:'900px', marginLeft:'150px'}}>
-      <h1>Meeting Hours Chart</h1>
-      <Bar data={chartData} options={chartOptions} />
+    <div>
+      <ChartMeeting/>
+      <StatisticItem/>
     </div>
-  );
-};
-
-export default Statistics;
-
+  )
+}
