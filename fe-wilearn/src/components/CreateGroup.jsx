8764dc92bbe8603b9fc3fa7b61eae786bbd1ff3f
builinht <90827474+builinht@users.noreply.github.com>
2024-03-10 20:31:57 +0700

    Create group

---
+++ fe-wilearn/src/components/CreateGroup.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+
+export default function CreateGroup() {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [groupName, setGroupName] = useState('');
+    const [subject, setSubject] = useState('');
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
+    const handleCreateGroup = () => {
+        console.log('Creating group:', { groupName, subject });
+        setOpenDialog(false);
+    };
+
+    return (
+        <>
+            <Button variant="outlined" size="small" onClick={() => setOpenDialog(true)}>
+                Create new group
+            </Button>
+
+            <Dialog open={openDialog} onClose={handleCloseDialog} maxWidth="xs" fullWidth>
+                <DialogTitle>Create New Group</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        label="Group Name"
+                        fullWidth
+                        sx={{ marginTop: '10px' }}
+                        value={groupName}
+                        onChange={(e) => setGroupName(e.target.value)}
+                    />
+                    <FormControl fullWidth sx={{ marginTop: '20px' }}>
+                        <InputLabel htmlFor="subject-label">Subject</InputLabel>
+                        <Select
+                            fullWidth
+                            labelId="subject-label"
+                            id="demo-simple-select"
+                            value={subject}
+                            label="Subject"
+                            onChange={(e) => setSubject(e.target.value)}
+                        >
+                            <MenuItem value="React">React</MenuItem>
+                            <MenuItem value="Python">Python</MenuItem>
+                            <MenuItem value="Java">Java</MenuItem>
+                        </Select>
+                    </FormControl>
+                </DialogContent>
+                <DialogActions style={{ padding: '16px' }}>
+                    <Button onClick={handleCloseDialog} color="secondary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleCreateGroup} color="primary">
+                        Create
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
