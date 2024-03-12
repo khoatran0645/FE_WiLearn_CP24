b5905db7833965f52ea9b7248a682a200f6344fb
PREDATOR\Owner <khoatran0645@gmail.com>
2024-03-12 16:34:24 +0700

    add drawer

--- fe-wilearn/src/layouts/MainLayout.jsx
+++ fe-wilearn/src/layouts/MainLayout.jsx
@@ -1,17 +1,20 @@
 import Grid from "@mui/material/Grid";
 import { Outlet } from "react-router-dom";
 import NavbarMenu from "../components/Nabar/NavbarMenu";
+import Drawer from "../components/Navbar/Drawer";
 
 export default function MainLayout() {
   return (
-    <Grid container spacing={12} sx={{ flexGrow: 1 }}>
-      <Grid item xs={12} sm={12} md={12} lg={12}>
+    <Grid container paddingTop={12} sx={{ flexGrow: 1 }}>
+      <Grid item xs={12}>
         <NavbarMenu />
       </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={12}>
+      <Grid item xs={2}>
+        <Drawer />
+      </Grid>
+      <Grid item xs={10}>
         <Outlet />
       </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={12}></Grid>
     </Grid>
   );
 }
