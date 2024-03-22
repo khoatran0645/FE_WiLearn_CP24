942b2b0e877440bd07e4c7f84a208a9f159d6917
Duy Lieng <duylieng.ntt@gmail.com>
2024-03-22 09:21:22 +0700

    modify text button

--- fe-wilearn/src/components/InviteUser.jsx
+++ fe-wilearn/src/components/InviteUser.jsx
@@ -41,7 +41,7 @@ export default function InviteUser() {
             Cancel
           </Button>
           <Button onClick={handleSearchSubmit} color="primary">
-            Search
+            Invite
           </Button>
         </DialogActions>
       </Dialog>
